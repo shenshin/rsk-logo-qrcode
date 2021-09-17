dd2dcdef0d5eee6af0f0fcb39b7cd9da4a168616
Aleksandr Shenshin <shenshin@me.com>
2021-09-17 16:41:17 +0200

    Add more logos and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,22 @@
+/* eslint-disable no-unused-vars */
 import { useState, useRef, useEffect } from 'react';
-import './App.css';
 import QRCodeStyling from 'qr-code-styling';
-import rskLogo from './rsk_logo.svg';
+import styles from './App.module.css';
+import graphics from './graphics';
 
 const MIN_QR_SIZE = 100; // px
 const MAX_QR_SIZE = 1000; // px
 const MIN_IMG_SIZE = 0.1;
 const MAX_IMG_SIZE = 0.5;
 
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    gap: '0.5rem',
-  },
-  title: {
-    marginBottom: 0,
-  },
-  rskImage: {
-    marginTop: '-1rem',
-    width: 100,
-    height: 100,
-  },
-  button: {
-    cursor: 'pointer',
-  },
-  error: {
-    color: 'red',
-    fontSize: '1.5rem',
-  },
-  numberInput: {
-    width: '3rem',
-  },
-  sourceInput: {
-    width: '50vw',
-  },
-};
-
 function App() {
+  const [currentLogo, setCurrentLogo] = useState(0);
   const [qrSource, setQrSource] = useState('https://www.rsk.co/');
   const [hasLogo, setHasLogo] = useState(true);
+  const [isStyled, setIsStyled] = useState(true);
   const [qrSize, setQrSize] = useState(200);
   const [imageSize, setImageSize] = useState(0.5);
+  const [error, setError] = useState('');
 
   const [qrCode] = useState(new QRCodeStyling());
   const qrRef = useRef();
@@ -67,22 +41,23 @@ function App() {
 
   const getOptions = () => {
     const { newQrSize, newImageSize } = validateInputs();
+    const logo = graphics[currentLogo];
     return {
       width: newQrSize,
       height: newQrSize,
       type: 'svg',
       data: qrSource,
-      image: hasLogo ? rskLogo : undefined,
+      image: hasLogo && `${process.env.PUBLIC_URL}/images/${logo.image}`,
       imageOptions: {
         imageSize: newImageSize,
-        margin: 5,
+        margin: logo.margin,
       },
       cornersSquareOptions: {
-        color: '#0b5d2e',
-        type: 'extra-rounded',
+        color: isStyled && logo.colors.dark,
+        type: isStyled && 'extra-rounded',
       },
       cornersDotOptions: {
-        color: '#16a92e',
+        color: isStyled && logo.colors.bright,
       },
     };
   };
@@ -96,6 +71,7 @@ function App() {
 
   const downloadImage = () => {
     qrCode?.download({
+      name: qrSource,
       extension: 'png',
     });
   };
@@ -108,12 +84,29 @@ function App() {
 
   useEffect(() => {
     updateOptions();
-  }, []);
+  }, [currentLogo, isStyled, hasLogo]);
 
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>Generate QR-code with RSK image</h1>
-      <img style={styles.rskImage} alt="RSK logo" src={rskLogo} />
+    <div className={styles.container}>
+      <h1 className={styles.title}>Generate QR-codes with IOVLabs logos</h1>
+      <div className={styles.iconsContainer}>
+        {graphics.map((logo, i) => (
+          <div className={styles.icon} key={logo.name}>
+            <img
+              src={`${process.env.PUBLIC_URL}/images/${logo.image}`}
+              className={styles.rskImage}
+              alt={`${logo.name} logo`}
+            />
+            <input
+              checked={i === currentLogo}
+              type="radio"
+              name="logo"
+              value={i}
+              onChange={(e) => setCurrentLogo(Number(e.target.value))}
+            />
+          </div>
+        ))}
+      </div>
       <label htmlFor="qr-source">
         String to encode
         <input
@@ -121,12 +114,12 @@ function App() {
           type="url"
           value={qrSource}
           onChange={(e) => setQrSource(e.target.value)}
-          style={styles.sourceInput}
+          className={styles.sourceInput}
           onKeyPress={handleEnter}
         />
       </label>
       <label htmlFor="place-logo">
-        Place RSK logo
+        {`Place ${graphics[currentLogo].name} logo`}
         <input
           type="checkbox"
           id="place-logo"
@@ -135,15 +128,27 @@ function App() {
           onKeyPress={handleEnter}
         />
       </label>
+      <label htmlFor="is-styled">
+        Styled QR code
+        <input
+          type="checkbox"
+          id="is-styled"
+          checked={isStyled}
+          onChange={(e) => setIsStyled(e.target.checked)}
+          onKeyPress={handleEnter}
+        />
+      </label>
       <label htmlFor="qr-size">
         {`QR size (${MIN_QR_SIZE}px - ${MAX_QR_SIZE}px)`}
         <input
           type="number"
+          min={MIN_QR_SIZE}
+          max={MAX_QR_SIZE}
           step="10"
           id="qr-size"
           value={qrSize}
           onChange={(e) => setQrSize(e.target.value)}
-          style={styles.numberInput}
+          className={styles.numberInput}
           onKeyPress={handleEnter}
         />
       </label>
@@ -151,24 +156,22 @@ function App() {
         {`Image size (${MIN_IMG_SIZE} - ${MAX_IMG_SIZE})`}
         <input
           type="number"
-          step="0.01"
+          min={MIN_IMG_SIZE}
+          max={MAX_IMG_SIZE}
+          step="0.05"
           id="image-size"
           value={imageSize}
           onChange={(e) => setImageSize(e.target.value)}
-          style={styles.numberInput}
+          className={styles.numberInput}
           onKeyPress={handleEnter}
         />
       </label>
-      <button
-        type="button"
-        onClick={updateOptions}
-        style={styles.button}
-        onKeyPress={handleEnter}
-      >
-        Generate a QR code
+      {error && <p>{error}</p>}
+      <button type="button" onClick={updateOptions} onKeyPress={handleEnter}>
+        Update QR code
       </button>
       <div ref={qrRef} />
-      <button type="button" onClick={downloadImage} style={styles.button}>
+      <button type="button" onClick={downloadImage}>
         Download PNG
       </button>
     </div>
---
+++ src/graphics.js
@@ -0,0 +1,39 @@
+const graphics = [
+  {
+    name: 'RSK',
+    image: 'rsk.svg',
+    margin: 4,
+    colors: {
+      bright: '#16a92e',
+      dark: '#0b5d2e',
+    },
+  },
+  {
+    name: 'IOVLabs',
+    image: 'o-logo.svg',
+    margin: 0,
+    colors: {
+      bright: '#1acd6a',
+      dark: '#0b5d2e',
+    },
+  },
+  {
+    name: 'RIF',
+    image: 'rif.svg',
+    margin: 4,
+    colors: {
+      bright: '#1ab1e3',
+      dark: '#0d57bd',
+    },
+  },
+  {
+    name: 'TARINGA',
+    image: 'taringa.svg',
+    margin: 2,
+    colors: {
+      bright: '#22509d',
+      dark: '#22509d',
+    },
+  },
+];
+export default graphics;
