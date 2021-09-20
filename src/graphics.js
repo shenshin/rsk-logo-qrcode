eac00e76c2a193f64903236304443b750dd25ed1
Aleksandr Shenshin <shenshin@me.com>
2021-09-21 01:24:35 +0200

    Improve input fields validation

--- src/graphics.js
+++ src/graphics.js
@@ -7,6 +7,7 @@ const graphics = [
       bright: '#1acd6a',
       dark: '#0b5d2e',
     },
+    url: 'https://www.iovlabs.org/',
   },
   {
     name: 'RSK',
@@ -16,6 +17,7 @@ const graphics = [
       bright: '#16a92e',
       dark: '#0b5d2e',
     },
+    url: 'https://www.rsk.co/',
   },
   {
     name: 'RIF',
@@ -25,6 +27,7 @@ const graphics = [
       bright: '#1ab1e3',
       dark: '#0d57bd',
     },
+    url: 'https://www.rifos.org/',
   },
   {
     name: 'TARINGA',
@@ -34,6 +37,7 @@ const graphics = [
       bright: '#22509d',
       dark: '#22509d',
     },
+    url: 'https://www.taringa.net/',
   },
 ];
 export default graphics;
